80c0493371055ad150d75e36f74a89a7bfce4d10
ShradhaPatil <patil.shradha0317@gmail.com>
2026-06-13 16:28:25 +0530

    -StudentEndPoints

--- src/main/java/com/shradha/student_result_management_system/student/StudentController.java
+++ src/main/java/com/shradha/student_result_management_system/student/StudentController.java
@@ -4,6 +4,9 @@ package com.shradha.student_result_management_system.student;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +26,47 @@ public class StudentController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-//    @GetMapping("/getAllStudent")
-// public ResponseEntity<Page<StudentResponseDTO>>g
+    @GetMapping("/getAllStudent")
+ public ResponseEntity<Page<StudentResponseDTO>> getAllStudent(
+         @RequestParam(defaultValue = "0") int page,
+         @RequestParam(defaultValue = "10") int size,
+         @RequestParam(defaultValue = "id") String sortBy,
+         @RequestParam(defaultValue = "asc") String sortDir
+    )
+    {
+
+        Sort sort =sortDir.equalsIgnoreCase("desc")?Sort.by(sortBy).descending() : Sort.by(sortBy).ascending();
+
+        Pageable pageable= PageRequest.of(page,size,sort);
+        Page<StudentResponseDTO> student=studentService.getAllStudents(pageable);
+        return ResponseEntity.ok(student);
+
+
+    }
+
+    @GetMapping("/{id}")
+
+    public ResponseEntity<StudentResponseDTO> getStudentBYId(@PathVariable Long id){
+         StudentResponseDTO Response= studentService.getStudentById(id);
+         return ResponseEntity.ok(Response);
+
+    }
+
+   @GetMapping("/update/{id}")
+    public ResponseEntity<StudentResponseDTO> updateStudent(@PathVariable Long id, @RequestBody @Valid StudentRequestDTO studentRequestDTO){
+
+
+       StudentResponseDTO respone= studentService.updateStudent(id,studentRequestDTO);
+       return ResponseEntity.ok(respone);
+
+   }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStudent(@PathVariable Long id) {
+
+        studentService.deleteStudent(id);
+        return ResponseEntity.noContent().build();
+
+    }
 }
